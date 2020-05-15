0027b3f1a4efb78868935ab4c46c5003c7fbac05
clement <clemsy@gmail.com>
2020-05-15 08:40:05 +0200

    element api
    added json api

---
+++ config/element-api.php
@@ -0,0 +1,39 @@
+<?php
+
+use craft\elements\Entry;
+use craft\helpers\UrlHelper;
+
+return [
+    'endpoints' => [
+        'news.json' => function() {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['section' => 'news'],
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'title' => $entry->title,
+                        'url' => $entry->url,
+                        'jsonUrl' => UrlHelper::url("news/{$entry->id}.json"),
+                        'summary' => $entry->newsSummary,
+                        'picture' => $entry->pageBanner,
+                    ];
+                },
+            ];
+        },
+        'news/<entryId:\d+>.json' => function($entryId) {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['id' => $entryId],
+                'one' => true,
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'title' => $entry->title,
+                        'url' => $entry->url,
+                        'summary' => $entry->newsSummary,
+                        'body' => $entry->body,
+                    ];
+                },
+            ];
+        },
+    ]
+];
